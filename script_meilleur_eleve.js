384410a134a6fd7688d22be5683846cc26e74b9a
LONCKE YANN <yann.loncke31@gmail.com>
2026-04-14 15:59:10 +0200

    script meilleur eleve mention

--- script_meilleur_eleve.js
+++ script_meilleur_eleve.js
@@ -1,18 +1,47 @@
 function analyseEleve(eleves){
     let bestStudent = eleves[0];
+    let worstStudent = eleves[0];
     let pass = 0;
     let recale = 0;
+    let mentions = [];
     if(eleves.length === 0){
         return("Erreur : Aucun élève !!!")
     }
     for(let i = 0; i < eleves.length; i++ ){
-        if (eleves[i].note > bestStudent.note){
-            bestStudent = eleves[i]
+        let eleve = eleves[i];
+        if (eleve.note > bestStudent.note){
+            bestStudent = eleve
         }
-        if (eleves[i].note >= 10){
+        if (eleve.note < worstStudent.note){
+            worstStudent = eleve
+        }
+        if (eleve.note >= 10){
             pass++
         } else{
             recale++
+        }
+        if(eleve.note >= 10){
+            if (eleve.note === 10){
+                mentions.push({
+                    nom : eleve.nom,
+                    mention : "Passable",
+                })
+            } else if (eleve.note < 16){
+                mentions.push({
+                    nom : eleve.nom,
+                    mention : "Bien",
+                })
+            } else {
+                mentions.push({
+                    nom : eleve.nom,
+                    mention : "Trés bien",
+                })
+            }
+        } else {
+            mentions.push({
+                nom : eleve.nom,
+                mention : "Insuffisant",
+            })
         }
     }
     const somme = eleves.reduce((acc, val) => acc + val.note, 0)
@@ -20,7 +49,9 @@ function analyseEleve(eleves){
     return{
         moyenne,
         bestStudent,
+        worstStudent,
         pass,
         recale,
+        mentions,
     }
 }
