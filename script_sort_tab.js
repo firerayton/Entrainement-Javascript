45559c7568d8be899c66b3e84712ed8c26a13379
LONCKE YANN <yann.loncke31@gmail.com>
2026-04-14 13:49:40 +0200

    script sort tab maj

--- script_sort_tab.js
+++ script_sort_tab.js
@@ -1,28 +1,37 @@
 function analyseTableau(tab){
     let positifs = 0;
     let negatifs = 0;
+    let nul = 0;
     let pairs = 0;
     let impairs = 0;
     for (let i = 0; i < tab.length; i++){
         let n = tab[i]
-        if (typeof(tab[i]) !== "number"){
+        if (typeof(n) !== "number"){
             return("Erreur : veuillez entrer uniquement des nombres.")
         }
         if (n < 0){
             negatifs++
-        }
-        if (n > 0) {
+        } else if (n > 0) {
             positifs++
-        }
+        } else
+            nul++
         if (n % 2 === 0){
             pairs++
         } else
             impairs++
     }
+    const max = Math.max(...tab);
+    const min = Math.min(...tab);
+    const somme = tab.reduce((acc, val) => acc + val, 0);
+    const moyenne = somme / tab.length ;
     return {
         positifs,
         negatifs,
+        nul,
         pairs,
-        impairs
+        impairs,
+        max,
+        min,
+        moyenne,
     }
 }
